d28193a589496fecfdc4a9eb68d9d2e7d620e756
Floubeur <guillaume@clic.me>
2019-08-17 10:19:46 +0200

    first push

---
+++ server-data/resources/esx_mission_illegal/__resource.lua
@@ -0,0 +1,20 @@
+description 'ESX Blanchimment d\'argent'
+
+
+client_scripts {
+  '@es_extended/locale.lua',
+  'locales/fr.lua',
+  'config.lua',
+  'client/main.lua',
+  'client/Mission/gofast.lua',
+  'client/Mission/killNPC.lua',
+  'client/Mission/killPLAYER.lua'
+}
+
+server_scripts {
+  '@mysql-async/lib/MySQL.lua',
+  '@es_extended/locale.lua',
+  'locales/fr.lua',
+  'config.lua',
+  'server/main.lua'
+}
---
+++ server-data/resources/esx_mission_illegal/locales/fr.lua
@@ -0,0 +1,39 @@
+Locales['fr'] = {
+
+  ['press_to_open'] = 'appuyez sur ~INPUT_CONTEXT~ pour accéder au menu',
+  ['lvl_robber']    = 'Bandit lvl : ',
+  ['mission_1']     = 'Faire un gofast',
+  ['mission_2']     = 'Meurte premier categorie',
+  ['mission_3']     = 'Meurte deuxième categorie',
+  ['mission_4']     = 'mission_4',
+  ['mission_5']     = 'mission_5',
+  ['create_robber'] = 'Devenir un bandit : ',
+
+  ------------------------------------------------------------------------------
+  ------------------------------------------------------------------------------
+  ['no_money']       = '~r~ Vous n\'avez pas ',
+  ['error']          = 'Action impossible',
+  ['Win']            = '~g~ Tu as gagnée : ',
+
+  ------------------------------------------------------------------------------
+  ------------------------------------------------------------------------------
+
+  ['Title_Gofast']    ='~r~ GO FAST ~d~',
+  ['Start_Gofast']    = 'Vas chercher la cam au plus vite',
+  ['Warning']         = '~y~ Attention à la police ~d~',
+  ['Step_1_Gofast']   = 'Va livrer la cam au plus vite',
+  ['caution']         = 'Tu as une caution de : ~r~',
+
+  ------------------------------------------------------------------------------
+  ------------------------------------------------------------------------------
+
+  ['Title_KillNPC']   = 'Contrat d\'élimination',
+  ['Start_KillNPC']   = 'Trouve et éllimine cette personne sous protection',
+  ['Win_KillNPC']     = 'Mission fini !'
+
+}
+
+
+
+
+
